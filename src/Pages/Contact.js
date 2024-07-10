4587c6e1d13c3e4231d31035bca55272a646cc6a
Boong45 <huyp0577@gmail.com>
2024-07-10 08:26:05 +0700

    tranggioithieu

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import FormContact from '../Components/FormContact';
+
 
 function Contact() {
   return (
     <div>
       <h1>Liên hệ</h1>
+
       {/* Thông tin liên hệ, mạng xã hội, cơ sở công ty */}
+
+      <FormContact/>
     </div>
   );
 }
